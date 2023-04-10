acd330b626e7b00e8ee0b629c94b4598c8de8dd3
Rohit Sawant <rohit.sawant@NTPL-IT-MAC-LAP0528.local>
2023-04-10 17:51:31 +0530

    FE changes

---
+++ ui/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { GamePageComponent } from './components/game-page/game-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'game/:id',
+    component: GamePageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ ui/src/app/constant.ts
@@ -0,0 +1,3 @@
+export const PAYMENT_LINKS = {
+  abi: [],
+};
